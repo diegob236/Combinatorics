3fc9fd3c2b05e818bbe00b563e44bb1e0f1e9d8a
Diego Batres <diegob236@gmail.com>
2017-10-27 00:29:41 -0600

    numnine.py - fixed negative numbers
    Added exception to prevent errors for negative numbers

--- numnine.py
+++ numnine.py
@@ -1,6 +1,9 @@
 import os
 
 
+class NegException(Exception): pass
+
+
 def numnine(n):
     if n == 0 : return 0
     else: return numnine(n-1)*9 + 10**(n-1)
@@ -15,10 +18,12 @@ if __name__ == '__main__':
         while True:
             try:
                 n = int(input())
+                if n < 0: raise NegException
                 while n > 997:
                     print(2*RESET + "Please enter a smaller number:")
                     n = int(input())
                 break
+            except NegException: print(2*RESET+"Enter a positive number:")
             except: print(2*RESET+"Enter a valid number:")
         if n < 15:
             print("Numbers containing digit 9 in range 0 to", 10**n - 1, ":", numnine(n))
