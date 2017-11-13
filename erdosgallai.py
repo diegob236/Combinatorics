a81fa7dbb1744ac46786f8888823e5f5c8c5f30b
Diego Batres <diegob236@gmail.com>
2017-11-12 21:14:22 -0700

    erdosgallai.py - added documentation
    Added comments explaining the code.

--- erdosgallai.py
+++ erdosgallai.py
@@ -1,57 +1,108 @@
-import os
+'''
+ERDOS-GALLAI
+
+Checks whether a list of numbers is a valid degree sequence using the Erdos-Gallai theorem.
+
+Author: Diego Batres
+Last updated: 11/12/17
+'''
 
 
+# NegException
+#   handles negative numbers, which are invalid degrees.
 class NegException(Exception): pass
 
 
+# Erdos-Gallai
+#   parameter: a list of positive degrees (ex. [2, 2, 2, 2, 2, 2])
+#   - Uses Erdos-Gallai theorem to validate if these numbers form a degree sequence.
+#   - Prints terms for each iteration
+#   returns: true if the given input is a valid degree sequence
 def erdosgallai(inp):
+
+    # k : k=1 to n
     k = 1
+    # n : total number of degrees
     n = len(inp)
+
+    # degseq : stores return value
     degseq = True
-    sumdi, summin, term2 = 0, 0, 0
+
+    # sumdi : sum of every degree di from i=1 to k
+    # summin : sum of min(di, k) from i=k+1 to n
+    sumdi, summin = 0, 0
+
+    # For each degree di in the given degree list
     for di in inp:
+
+        # Add di to the total sum
         sumdi += di
+
+        # Calculate sum of min(di, k) from i=k+1 to n
         for i in range(k + 1, n + 1):
-            # print("di,k =", inp[i-1], k)
-            # print("min(di,k)=", min(inp[i-1], k))
             summin += min(inp[i - 1], k)
-        # print("summin:", summin)
+
+        # Calculate k(k - 1)
         kk = k * (k - 1)
+
+        # Print the values of each term
         print("k:", k, "=", di, "    Sum(di)i->k  = ", sumdi,
               "     k(k-1) + Sum(min(di,k))i=k+1->n  = ", kk, "+", summin, "=", kk + summin,
               "   ", sumdi, "≤", kk + summin, ":", bool(sumdi <= (kk + summin)))
+
+        # Check if di satisfies the conditions of Erdos-Gallai
         if sumdi > (kk + summin):
             degseq = False
+
+        # Increment k and reset value of summin
         k += 1
         summin = 0
+
+    # Return true if input is a valid degree sequence
     return degseq
 
+
+# Main method
+#   input: nonnegative numbers separated by spaces (Ex. 2 2 2 2 2 2)
+#   - Uses the Erdos-Gallai theorem to check if these numbers are a degree sequence
+#   - Prints result
 if __name__ == "__main__":
+
+    # Erases a line of terminal output
+    #   - Used for changing messages when an invalid input is given
     RESET = '\x1b[1A' + '\x1b[2K'
+
+    # Print header
+    print("---------ERDOS-GALLAI---------")
+    print("checks whether a list of numbers is a valid degree sequence using the Erdos-Gallai theorem.\n")
+    print("Enter degrees separated by spaces:")
+
+    # Check for input until a valid sequence of numbers is given
     while True:
-        print("---------ERDOS-GALLAI---------")
-        print("checks whether a list of numbers is a valid degree sequence using the Erdos-Gallai theorem.\n")
-        print("Enter degrees separated by spaces:")
-        while True:
-            try:
-                inp = list(map(int, input().split()))
-                for i in inp:
-                    if i < 0: raise NegException
-                break
-            except NegException:
-                print(2*RESET+"Enter only positive numbers:")
-            except ValueError:
-                print(2*RESET+"Enter only numbers separated by spaces:")
-        inp = sorted(inp, key=int, reverse=True)
-        print(inp, "\n")
-        degseq = erdosgallai(inp)
-        print("")
-        print(inp, ("is" if degseq else "is not"), "a degree sequence"+("!" if degseq else "."))
-        c = ''
-        print("\n\n")
-        while c != 'y' and c != 'n':
-            print(2 * RESET + "Try again? (y/n):")
-            c = input()
-        os.system('cls' if os.name == 'nt' else 'clear')
-        if c == 'y': print("")
-        if c == 'n': break
+
+        # Store numbers in a list if valid
+        try:
+            inp = list(map(int, input().split()))
+            for i in inp:
+                if i < 0: raise NegException
+            break
+
+        # Prompt user for only positive numbers if a negative value is given
+        except NegException:
+            print(2 * RESET + "Enter only positive numbers:")
+
+        # Prompt user for only numbers separated by spaces if the wrong input is given
+        except ValueError:
+            print(2 * RESET + "Enter only numbers separated by spaces:")
+
+    # Sort degrees from highest to lowest
+    inp = sorted(inp, key=int, reverse=True)
+
+    # Print sorted list of numbers
+    print(inp, "\n")
+
+    # Check if the degree sequence is valid
+    degseq = erdosgallai(inp)
+
+    # Print results
+    print("\n", inp, ("is" if degseq else "is not"), "a degree sequence" + ("!" if degseq else "."))
